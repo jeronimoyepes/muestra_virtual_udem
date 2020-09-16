32941032dec54c5f593ceb298116bc6654f730fa
Jeró <jeronimo_y_g@hotmail.com>
2020-09-16 04:44:12 -0500

    fugas

---
+++ dist/js/interaction.js
@@ -0,0 +1,28 @@
+$(".step-controller li").click(function (e) { 
+    
+    currentStep = $(this).attr("step")
+    
+    if (currentStep == "a") {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active")
+        $(this).closest(".three-column").children(".step").hide();
+        $(this).closest(".three-column").children(".a").show();
+        $(this).closest("section").children(".overflowed").children("img:nth-child(1)").show();
+        $(this).closest("section").children(".overflowed").children("img:nth-child(2), img:nth-child(3)").hide();
+    } else if (currentStep == "b") {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active")
+        $(this).closest(".three-column").children(".step").hide();
+        $(this).closest(".three-column").children(".b").show();
+        $(this).closest("section").children(".overflowed").children("img:nth-child(2)").show();
+        $(this).closest("section").children(".overflowed").children("img:nth-child(1), img:nth-child(3)").hide();
+    } else {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active")
+        $(this).closest(".three-column").children(".step").hide();
+        $(this).closest(".three-column").children(".c").show();
+        $(this).closest("section").children(".overflowed").children("img:nth-child(3)").show();
+        $(this).closest("section").children(".overflowed").children("img:nth-child(1), img:nth-child(2)").hide();
+    }
+
+});
--- dist/js/styling.js
+++ dist/js/styling.js
@@ -3,4 +3,7 @@ const images = document.querySelectorAll("img");
 for (let i = 0; i < images.length; i++) {
     imgWidth = images[i].getAttribute("img-width");
     images[i].style.maxWidth = imgWidth + 'px';
+
+    imgWidth = images[i].getAttribute("img-min-width");
+    images[i].style.minWidth = imgWidth + 'px';
 }
